d53c80e25ac1c33689b97c4404d80db020e6e089
oms <omsm66@gmail.com`>
2025-06-01 05:34:41 -0700

    code2.js

---
+++ code2.js
@@ -0,0 +1,12 @@
+
+let num=[10,-5,0,42,-100]
+
+    if (num > 0) {
+        console.log('positive');
+    } else if (num < 0) {
+        console.log('negative');
+    } else {
+        console.log('zero');
+    }
+
+console.log(2);
