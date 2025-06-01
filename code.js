cb0d8f322968bb46ccea82cc630ccc5a197f0d82
oms <omsm66@gmail.com`>
2025-06-01 05:16:31 -0700

    code.js

---
+++ code.js
@@ -0,0 +1,16 @@
+
+    num1 = 10
+    num2 = 20
+
+    
+    let addition = num1 + num2
+    //let subtraction = num1 - num2
+   // let multiplication = num1 ** num2
+    let division = num1 / num2
+       let  modulus = num1 % num2
+
+    console.log(addition);
+    
+    
+   
+    
